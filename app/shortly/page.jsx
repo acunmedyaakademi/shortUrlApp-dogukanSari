5ac78bbba869624ad67c61e82d0e96a789b22d0d
Doğukan Sarı <teddogukan@gmail.com>
2024-10-04 17:24:48 +0300

    fetch ile supabasedeki veriler çekildi.

--- app/shortly/page.jsx
+++ app/shortly/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/router";
-import { useState } from "react/cjs/react.production.min";
+import { useEffect, useState } from "react/cjs/react.production.min";
 
 export default function Shortly() {
   const [data, setData] = useState(null);
@@ -19,6 +19,33 @@ export default function Shortly() {
       alert("Link copied to clipboard!");
     }
   }
+
+  const fetchedData = async() => {
+    const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4ZG1hZG9weWxxendkZHZ4bmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5NjY1NjEsImV4cCI6MjA0MzU0MjU2MX0.VWXtziyfAVzJTz-M8iqND-_hgE69Nmzb6e8ZpGx36DE"
+    const apiUrl = "https://yxdmadopylqzwddvxncx.supabase.co/rest/v1/urls?select=*"
+
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        apiKey: apiKey,
+        Authorization: `Bearer ${apiKey}`,
+      }
+    });
+
+    const resData = await res.json();
+
+    if (resData) {
+      const newData = resData.find((x) => x.long_url === longUrl);
+      setData(newData);
+    }
+    return setProducts(resData);
+  }
+
+  useEffect(() => {
+    fetchedData();
+  }, []);
+
+  
   return (
     <>
       <div className="formContainer">
