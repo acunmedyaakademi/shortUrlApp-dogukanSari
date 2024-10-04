9ec84cc426abdc0e26dc67d20c60754b24c604ab
Doğukan Sarı <teddogukan@gmail.com>
2024-10-04 17:19:43 +0300

    Shorten it butonuna basıldığında longUrl'i kaydeden fonksiyon yazıldı. Copy butonuna basıldığında kısa linki kopyalayan fonksiyon yazıldı.

--- app/shortly/page.jsx
+++ app/shortly/page.jsx
@@ -1,5 +1,24 @@
 "use client";
+
+import { useRouter } from "next/router";
+import { useState } from "react/cjs/react.production.min";
+
 export default function Shortly() {
+  const [data, setData] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [longUrl, setLongUrl] = useState(null);
+  const router = useRouter(null);
+
+  const findShortUrl = () => {
+    insertData(longUrl, makeid(7));
+  }
+
+  const copyToClipboard = () => {
+    if (data?.short_url) {
+      navigator.clipboard.writeText(data.short_url);
+      alert("Link copied to clipboard!");
+    }
+  }
   return (
     <>
       <div className="formContainer">
@@ -9,12 +28,12 @@ export default function Shortly() {
             placeholder="Shorten a link here..."
             value={longUrl}
           />
-          <button>Shorten It!</button>
+          <button onClick={() => findShortUrl()}>Shorten It!</button>
         </div>
         <div className="shortUrl">
           <p>Buraya short_url gelecek</p>
           <div className="buttons">
-            <button>Copied</button>
+            <button onClick={copyToClipboard}>Copied</button>
             <button>Go To Url</button>
           </div>
         </div>
